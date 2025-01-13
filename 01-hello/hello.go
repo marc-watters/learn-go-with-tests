6e3a45ed9ca06bfc9470b285049bc04b5b24b3ce
Marc Watters <marc.watters@proton.me>
2025-01-13 14:54:55 -0500

    01 - fix: use default name if empty string passed to Hello()
    asd

--- 01-hello/hello.go
+++ 01-hello/hello.go
@@ -5,5 +5,9 @@ import "fmt"
 const englishPrefix = "Hello"
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+
 	return fmt.Sprintf("%s, %s!", englishPrefix, name)
 }
--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -3,10 +3,21 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Marc")
-	want := "Hello, Marc!"
+	t.Run("say hello by name", func(t *testing.T) {
+		got := Hello("Marc")
+		want := "Hello, Marc!"
 
-	if got != want {
-		t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
-	}
+		if got != want {
+			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
+		}
+	})
+
+	t.Run("use default name if passed empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
+		}
+	})
 }
