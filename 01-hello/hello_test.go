b4747fc0472e98732afbe092292bca7418730659
Marc Watters <marc.watters@proton.me>
2025-01-13 14:46:52 -0500

    01 - feat: say hello by name
    This commit allows a name to be passed as an argument to the Hello()
    function.

--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -3,8 +3,8 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
+	got := Hello("Marc")
+	want := "Hello, Marc!"
 
 	if got != want {
 		t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
