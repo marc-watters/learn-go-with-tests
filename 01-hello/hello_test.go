ae401cd8c6c1cdcb6839a6d7ff61f163ff52f731
Marc Watters <marc.watters@proton.me>
2025-01-13 14:58:19 -0500

    01 - rfxr(test): add helper assertCorrectMessage()

--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -6,18 +6,20 @@ func TestHello(t *testing.T) {
 	t.Run("say hello by name", func(t *testing.T) {
 		got := Hello("Marc")
 		want := "Hello, Marc!"
-
-		if got != want {
-			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("use default name if passed empty string", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
+	}
 }
