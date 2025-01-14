45b9ae35811a1b796ffbf08c39debba70612a2d1
Marc Watters <marc.watters@proton.me>
2025-01-14 11:49:54 -0500

    04 - feat: implement SumAll()

--- 04-arrays_slices/sum_test.go
+++ 04-arrays_slices/sum_test.go
@@ -1,6 +1,9 @@
 package collections
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3}
@@ -9,6 +12,15 @@ func TestSum(t *testing.T) {
 	want := 6
 
 	if got != want {
+		t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+	want := []int{6, 15}
+
+	if !slices.Equal(got, want) {
 		t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
 	}
 }
