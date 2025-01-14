fb33df277e07cf9df765426a05f3655bf1aed365
Marc Watters <marc.watters@proton.me>
2025-01-14 11:42:50 -0500

    04 - rfxr: use slices over arrays

--- 04-arrays_slices/sum_test.go
+++ 04-arrays_slices/sum_test.go
@@ -3,12 +3,12 @@ package collections
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3}
 
 	got := Sum(numbers)
-	want := 15
+	want := 6
 
 	if got != want {
-		t.Errorf("\ngot: \t%d\nwant:\t%d\ngiven:\t%v", got, want, numbers)
+		t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
 	}
 }
