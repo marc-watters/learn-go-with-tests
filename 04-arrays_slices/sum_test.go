4b019419877b5aa5e34b44efce680c48e2a6ea91
Marc Watters <marc.watters@proton.me>
2025-01-14 12:12:53 -0500

    04 - fix: handle empty slices

--- 04-arrays_slices/sum_test.go
+++ 04-arrays_slices/sum_test.go
@@ -26,15 +26,25 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails(
-		[]int{1, 2, 3},
-		[]int{4, 5, 6},
-		[]int{7, 8, 9},
-	)
+	t.Run("sum tails of varying slices", func(t *testing.T) {
+		got := SumAllTails(
+			[]int{1, 2, 3},
+			[]int{4, 5, 6},
+			[]int{7, 8, 9},
+		)
+		want := []int{5, 11, 17}
 
-	want := []int{5, 11, 17}
+		if !slices.Equal(got, want) {
+			t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
+		}
+	})
 
-	if !slices.Equal(got, want) {
-		t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
-	}
+	t.Run("with empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{})
+		want := []int{0}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
+		}
+	})
 }
