d4dbd0e695c1b635502e68242c978876c5f01d37
Marc Watters <marc.watters@proton.me>
2025-01-14 11:04:55 -0500

    03 - feat: repeat count passed as argument

--- 03-iteration/iteration_test.go
+++ 03-iteration/iteration_test.go
@@ -3,10 +3,17 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("\nrepeated:\t%q\nexpected:\t%q", repeated, expected)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	count := b.N
+	for i := 0; i < count; i++ {
+		Repeat("a", count)
 	}
 }
