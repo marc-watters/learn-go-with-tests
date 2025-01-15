3674bbb9ce0a082914c52a1f0d45cd5b19f8ad7c
Marc Watters <marc.watters@proton.me>
2025-01-15 14:10:42 -0500

    05 - feat: implement Perimeter()

---
+++ 05-structs_methods_interfaces/structs_methods_interfaces_test.go
@@ -0,0 +1,12 @@
+package structsmethodsinterfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("\ngot: \t%.2f\nwant:\t%.2f", got, want)
+	}
+}
