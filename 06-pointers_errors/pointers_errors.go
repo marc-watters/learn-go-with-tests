f44d7e4182396bb4527e8c2053299515471663c3
Marc Watters <marc.watters@proton.me>
2025-01-15 21:44:56 -0500

    06 - add: type BitCoin

--- 06-pointers_errors/pointers_errors.go
+++ 06-pointers_errors/pointers_errors.go
@@ -1,13 +1,15 @@
 package pointerserrors
 
+type BitCoin int
+
 type Wallet struct {
-	balance int
+	balance BitCoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount BitCoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() BitCoin {
 	return w.balance
 }
