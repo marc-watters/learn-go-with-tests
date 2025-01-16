6e634c4a3aaed5948e7ae08393f5edd0e152fffc
Marc Watters <marc.watters@proton.me>
2025-01-15 21:44:56 -0500

    06 - add: type BitCoin

--- 06-pointers_errors/pointers_errors_test.go
+++ 06-pointers_errors/pointers_errors_test.go
@@ -5,10 +5,10 @@ import "testing"
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(BitCoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := BitCoin(10)
 
 	if got != want {
 		t.Errorf("\ngot: \t%d\nwant:\t%d", got, want)
