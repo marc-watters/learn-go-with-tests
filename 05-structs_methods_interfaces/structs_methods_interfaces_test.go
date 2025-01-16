51bf57f08bd1fd548ada26d446df596d79b81d4b
Marc Watters <marc.watters@proton.me>
2025-01-15 19:04:23 -0500

    05 - rfxr(test): add helper checkArea()

--- 05-structs_methods_interfaces/structs_methods_interfaces_test.go
+++ 05-structs_methods_interfaces/structs_methods_interfaces_test.go
@@ -13,23 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 5.0}
-		got := rectangle.Area()
-		want := 25.0
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+
+		got := shape.Area()
 
 		if got != want {
 			t.Errorf("\ngot: \t%g\nwant:\t%g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{5.0, 5.0}
+		checkArea(t, rectangle, 25.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("\ngot: \t%g\nwant:\t%g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
