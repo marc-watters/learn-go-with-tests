37bbf6b454c938a39daea7394321c7e687e5bdc5
Marc Watters <marc.watters@proton.me>
2025-01-15 19:12:16 -0500

    05 - rfxr: utilize table driven tests

--- 05-structs_methods_interfaces/structs_methods_interfaces_test.go
+++ 05-structs_methods_interfaces/structs_methods_interfaces_test.go
@@ -13,23 +13,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("\ngot: \t%g\nwant:\t%g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{5.0, 5.0}, 25.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 5.0}
-		checkArea(t, rectangle, 25.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("\ngot: \t%g\nwant:\t%g", got, tt.want)
+		}
+	}
 }
