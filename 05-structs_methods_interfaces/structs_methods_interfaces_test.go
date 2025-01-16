d8c940750966a609b2779b42027692f6e512ad1d
Marc Watters <marc.watters@proton.me>
2025-01-15 19:23:41 -0500

    05 - rfxr: change test Shape field "want" to be more descriptive

--- 05-structs_methods_interfaces/structs_methods_interfaces_test.go
+++ 05-structs_methods_interfaces/structs_methods_interfaces_test.go
@@ -14,18 +14,18 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 5.0, Height: 5.0}, want: 25.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12.0, Height: 6.0}, want: 36.0},
+		{shape: Rectangle{Width: 5.0, Height: 5.0}, hasArea: 25.0},
+		{shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("\ngot: \t%g\nwant:\t%g", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("\ngot: \t%g\nwant:\t%g", got, tt.hasArea)
 		}
 	}
 }
