422d8e36ddffcbb2c76d609951bb672e90fb47a5
Marc Watters <marc.watters@proton.me>
2025-01-16 17:36:38 -0500

    06 - rfxr(test): implement helper assertBalance()

--- 06-pointers_errors/pointers_errors_test.go
+++ 06-pointers_errors/pointers_errors_test.go
@@ -3,31 +3,27 @@ package pointerserrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(BitCoin(10))
+	assertBalance := func(t testing.TB, wallet Wallet, want BitCoin) {
+		t.Helper()
 
 		got := wallet.Balance()
-		want := BitCoin(10)
-
-		wallet.Deposit(BitCoin(10))
 
 		if got != want {
 			t.Errorf("\ngot: \t%s\nwant:\t%s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		amount := BitCoin(10)
+		wallet.Deposit(amount)
+		assertBalance(t, wallet, amount)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: BitCoin(20)}
-
 		wallet.Withdraw(BitCoin(10))
-
-		got := wallet.Balance()
 		want := BitCoin(10)
-
-		if got != want {
-			t.Errorf("\ngot: \t%s\nwant:\t%s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
