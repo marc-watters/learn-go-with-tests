23c7bb3d2c7218fc56b67ca255edab7335600078
Marc Watters <marc.watters@proton.me>
2025-01-15 21:35:13 -0500

    06 - feat: type Wallet Deposit()
    06 - add: type Wallet Balance() method

--- 06-pointers_errors/pointers_errors.go
+++ 06-pointers_errors/pointers_errors.go
@@ -3,3 +3,11 @@ package pointers_errors
 type Wallet struct {
 	balance int
 }
+
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
