aa8658ca03e68af71d6e834ad8a01f20f243b0e9
Marc Watters <marc.watters@proton.me>
2025-01-15 21:52:47 -0500

    06 - feat: implement Wallet Withdraw()

--- 06-pointers_errors/pointers_errors.go
+++ 06-pointers_errors/pointers_errors.go
@@ -19,3 +19,7 @@ func (w *Wallet) Deposit(amount BitCoin) {
 func (w *Wallet) Balance() BitCoin {
 	return w.balance
 }
+
+func (w *Wallet) Withdraw(amount BitCoin) {
+	w.balance -= amount
+}
--- 06-pointers_errors/pointers_errors_test.go
+++ 06-pointers_errors/pointers_errors_test.go
@@ -3,14 +3,31 @@ package pointerserrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(BitCoin(10))
+		wallet.Deposit(BitCoin(10))
 
-	got := wallet.Balance()
-	want := BitCoin(10)
+		got := wallet.Balance()
+		want := BitCoin(10)
 
-	if got != want {
-		t.Errorf("\ngot: \t%s\nwant:\t%s", got, want)
-	}
+		wallet.Deposit(BitCoin(10))
+
+		if got != want {
+			t.Errorf("\ngot: \t%s\nwant:\t%s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: BitCoin(20)}
+
+		wallet.Withdraw(BitCoin(10))
+
+		got := wallet.Balance()
+		want := BitCoin(10)
+
+		if got != want {
+			t.Errorf("\ngot: \t%s\nwant:\t%s", got, want)
+		}
+	})
 }
