7a5db3304a1382cf36879ff937dbc555d86d00f1
Marc Watters <marc.watters@proton.me>
2025-01-22 23:16:33 -0500

    09 - rfxr: modify Countdown() to accept writer interface

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Countdown(out *bytes.Buffer) {
+func Countdown(out io.Writer) {
 	fmt.Fprint(out, "3")
 }
 
-func main() {}
+func main() {
+	Countdown(os.Stdout)
+}
