c0f38dbb86eae6310b6cb66bda817d3c3ef9f7bf
Marc Watters <marc.watters@proton.me>
2025-01-23 08:22:33 -0500

    09 - rfxr: implement Sleeper for mocking
    Adds types Sleeper interface, SpySleeper struct, and DefaultSleeper
    struct to support mocking.

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -12,14 +12,33 @@ const (
 	finalWord      = "Go!"
 )
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countDownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- 09-mocking/countdown_test.go
+++ 09-mocking/countdown_test.go
@@ -7,13 +7,18 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := "3\n2\n1\nGo!"
 
 	if got != want {
 		t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper\nwant:\t3\ngot: \t%d", spySleeper.Calls)
 	}
 }
