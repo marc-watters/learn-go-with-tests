1900cd43a2555b6d95d031c1f2ed9a5328ee460d
Marc Watters <marc.watters@proton.me>
2025-01-22 23:25:03 -0500

    09 - rfxr: add constants for coundown start and final word

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -6,11 +6,16 @@ import (
 	"os"
 )
 
+const (
+	countDownStart = 3
+	finalWord      = "Go!"
+)
+
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countDownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
