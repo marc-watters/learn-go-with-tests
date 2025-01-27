62955936c0505803f9572480713a8902344c8f36
Marc Watters <marc.watters@proton.me>
2025-01-27 04:46:02 -0500

    11 - rfxr: add helper measureResponseTime()

--- 11-racer/racer.go
+++ 11-racer/racer.go
@@ -8,22 +8,20 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	if _, err := http.Get(a); err != nil {
-		fmt.Fprintf(os.Stderr, "error getting url: %v", err)
-		os.Exit(1)
-	}
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	if _, err := http.Get(b); err != nil {
-		fmt.Fprintf(os.Stderr, "error getting url: %v", err)
-		os.Exit(1)
-	}
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	if _, err := http.Get(url); err != nil {
+		fmt.Fprintf(os.Stderr, "error getting url: %v", err)
+		os.Exit(1)
+	}
+	return time.Since(start)
 }
