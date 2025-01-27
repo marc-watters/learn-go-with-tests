1a07cb663bc63427f85f7c66312f8a8c45baf593
Marc Watters <marc.watters@proton.me>
2025-01-27 05:03:17 -0500

    11 - rfxr: timeout after 10 seconds

--- 11-racer/racer_test.go
+++ 11-racer/racer_test.go
@@ -8,20 +8,38 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20)
-	fastServer := makeDelayedServer(0)
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("compare speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20)
+		fastServer := makeDelayedServer(0)
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+		if err != nil {
+			t.Fatalf("error during test: %v", err)
+		}
 
-	if got != want {
-		t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
-	}
+		if got != want {
+			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
+		}
+	})
+
+	t.Run("return an error if server does not respond in 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Errorf("expected an error but did not receive one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
