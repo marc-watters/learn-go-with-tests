2769147798e535b1f66ae004d703b06a13c85aca
Marc Watters <marc.watters@proton.me>
2025-01-28 17:37:28 -0500

    12 - add: implement walk()

---
+++ 12-reflection/reflection_test.go
@@ -0,0 +1,24 @@
+package reflection
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	got := []string{}
+	expected := "Marc"
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("\ngot: \t%q\nwant:\t%q\n", got[0], expected)
+	}
+}
