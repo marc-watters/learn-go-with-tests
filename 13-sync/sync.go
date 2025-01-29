a72843ad619a1af6d7bf4dd0f19063957152b65f
Marc Watters <marc.watters@proton.me>
2025-01-28 23:57:27 -0500

    13 - fix: make Counter concurrency safe

--- 13-sync/sync.go
+++ 13-sync/sync.go
@@ -1,6 +1,16 @@
 package sync
 
-type Counter struct{ value int }
+import "sync"
 
-func (c *Counter) Inc()       { c.value++ }
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.value++
+	c.mu.Unlock()
+}
+
 func (c *Counter) Value() int { return c.value }
