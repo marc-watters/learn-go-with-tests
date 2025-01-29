cbbf9c431904b15371c6de573edb4d508a5deb6f
Marc Watters <marc.watters@proton.me>
2025-01-29 17:37:04 -0500

    15 - rfxr(test): implement table based tests

--- 15-property_based_testing/roman_numerals_test.go
+++ 15-property_based_testing/roman_numerals_test.go
@@ -3,21 +3,21 @@ package romannumerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("converts 1 to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"converts 1 to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
-		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("\ngot: \t%q\nwant:\t%q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("\ngot: \t%q\nwant:\t%q", got, test.Want)
+			}
+		})
+	}
 }
