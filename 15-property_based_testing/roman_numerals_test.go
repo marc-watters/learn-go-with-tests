8202b7afc345af9f60dd76760a0c534f98862b21
Marc Watters <marc.watters@proton.me>
2025-01-29 21:15:06 -0500

    15 - feat: implement ConvertToArabic()

--- 15-property_based_testing/roman_numerals_test.go
+++ 15-property_based_testing/roman_numerals_test.go
@@ -5,41 +5,53 @@ import (
 	"testing"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Roman  string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{7, "VII"},
-		{8, "VIII"},
-		{9, "IX"},
-		{10, "X"},
-		{14, "XIV"},
-		{18, "XVIII"},
-		{20, "XX"},
-		{39, "XXXIX"},
-		{40, "XL"},
-		{47, "XLVII"},
-		{49, "XLIX"},
-		{50, "L"},
-		{100, "C"},
-		{500, "D"},
-		{1000, "M"},
-		{1984, "MCMLXXXIV"},
-	}
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100, "C"},
+	{500, "D"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+}
 
+func TestRomanNumerals(t *testing.T) {
 	for _, test := range cases {
 		description := fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman)
 		t.Run(description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
 			if got != test.Roman {
 				t.Errorf("\ngot: \t%q\nwant:\t%q", got, test.Roman)
+			}
+		})
+	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases[:1] {
+		description := fmt.Sprintf("%s gets converted to %d", test.Roman, test.Arabic)
+		t.Run(description, func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("\ngot: \t%d\nwant:\t%d", got, test.Arabic)
 			}
 		})
 	}
