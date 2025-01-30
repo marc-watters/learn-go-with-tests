c29852a4826642f47e9951fb36f328957bcc97a7
Marc Watters <marc.watters@proton.me>
2025-01-30 17:20:13 -0500

    16 - add(test): implement TestMinutesInRadians()

--- 16-math/clockface/clockface.go
+++ 16-math/clockface/clockface.go
@@ -82,6 +82,22 @@ func secondHandPoint(t time.Time) Point {
 	return Point{x, y}
 }
 
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
 }
