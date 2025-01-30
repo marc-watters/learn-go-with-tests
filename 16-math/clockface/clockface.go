2f1d422ce9bd636d5590bfe39f9700f96d177b2d
Marc Watters <marc.watters@proton.me>
2025-01-30 17:32:46 -0500

    16 - add(test): implement TestSVGWriterHourHand()

--- 16-math/clockface/clockface.go
+++ 16-math/clockface/clockface.go
@@ -15,11 +15,12 @@ type Point struct {
 }
 
 const (
-	secondHandLength = 90
-	clockCentreX     = 150
-	clockCentreY     = 150
+	clockCentreX = 150
+	clockCentreY = 150
 
+	secondHandLength = 90
 	minuteHandLength = 80
+	hourHandLength   = 50
 )
 
 const (
@@ -52,6 +53,7 @@ func SVGWriter(w io.Writer, t time.Time) {
 
 	secondHand(w, t)
 	minuteHand(w, t)
+	hourHand(w, t)
 
 	_, err = io.WriteString(w, svgEnd)
 	if err != nil {
@@ -101,14 +103,28 @@ func minuteHandPoint(t time.Time) Point {
 	return angleToPoint(minutesInRadians(t))
 }
 
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) +
+		(math.Pi / (6 / float64(t.Hour()%12)))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
+
+func hourHand(w io.Writer, t time.Time) {
+	p := makeHand(hourHandPoint(t), hourHandLength)
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:3px;"/>`, p.X, p.Y)
+}
+
 func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
 	return Point{x, y}
-}
-
-func minutesInRadians(t time.Time) float64 {
-	return (secondsInRadians(t) / 60) +
-		(math.Pi / (30 / float64(t.Minute())))
 }
