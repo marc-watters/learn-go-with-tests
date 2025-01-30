b29851d8c44cc7fbef69a7315b71a990db1e57fd
Marc Watters <marc.watters@proton.me>
2025-01-30 16:48:04 -0500

    16 - add: implement SVGWriter()

--- 16-math/clockface/clockface.go
+++ 16-math/clockface/clockface.go
@@ -1,7 +1,10 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
+	"os"
 	"time"
 )
 
@@ -16,6 +19,50 @@ const (
 	clockCentreX     = 150
 	clockCentreY     = 150
 )
+
+const (
+	svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+	bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+	svgEnd = `</svg>`
+)
+
+// SVGWriter writes an SVG representation of an analogue clock, showing the
+// time t, to the writer w
+func SVGWriter(w io.Writer, t time.Time) {
+	var err error
+	_, err = io.WriteString(w, svgStart)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error writing clock start: %v", err)
+	}
+
+	_, err = io.WriteString(w, bezel)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error writing clock bezel: %v", err)
+	}
+
+	secondHand(w, t)
+
+	_, err = io.WriteString(w, svgEnd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error writing clock end: %v", err)
+	}
+}
+
+func secondHand(w io.Writer, t time.Time) {
+	p := secondHandPoint(t)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+}
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
