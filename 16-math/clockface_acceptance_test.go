617816f385d36379df344222895d7bd2fa5655dc
Marc Watters <marc.watters@proton.me>
2025-01-30 16:58:39 -0500

    16 - add(test): implement TestSVGWriterMinuteHand()

--- 16-math/clockface_acceptance_test.go
+++ 16-math/clockface_acceptance_test.go
@@ -112,6 +112,35 @@ func TestSVGWriterSecondHand(t *testing.T) {
 	}
 }
 
+func TestSVGWriterMinuteHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 70},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			err := xml.Unmarshal(b.Bytes(), &svg)
+			if err != nil {
+				t.Fatalf("error unmarhsalling minute hand: %v", err)
+			}
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
 func containsLine(l Line, ls []Line) bool {
 	for _, line := range ls {
 		if line == l {
