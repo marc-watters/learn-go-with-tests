8686573b60c1d3485e8337a287a4fbe7ed43faee
Marc Watters <marc.watters@proton.me>
2025-01-29 21:08:01 -0500

    15 - rfxr(test): modify test case struct for brevity

--- 15-property_based_testing/roman_numerals_test.go
+++ 15-property_based_testing/roman_numerals_test.go
@@ -1,43 +1,45 @@
 package romannumerals
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Roman  string
 	}{
-		{"converts 1 to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"6 gets converted to VI", 6, "VI"},
-		{"7 gets converted to VII", 7, "VII"},
-		{"8 gets converted to VIII", 8, "VIII"},
-		{"9 gets converted to IX", 9, "IX"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"39 gets converted to XXXIX", 39, "XXXIX"},
-		{"40 gets converted to XL", 40, "XL"},
-		{"47 gets converted to XLVII", 47, "XLVII"},
-		{"49 gets converted to XLIX", 49, "XLIX"},
-		{"50 gets converted to L", 50, "L"},
-		{"100 gets converted to C", 100, "C"},
-		{"500 gets converted to D", 500, "D"},
-		{"1000 gets converted to M", 1000, "M"},
-		{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{100, "C"},
+		{500, "D"},
+		{1000, "M"},
 		{1984, "MCMLXXXIV"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		description := fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman)
+		t.Run(description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("\ngot: \t%q\nwant:\t%q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("\ngot: \t%q\nwant:\t%q", got, test.Roman)
 			}
 		})
 	}
