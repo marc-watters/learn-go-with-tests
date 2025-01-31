7d2adde69dbc2dda3dd07239111d1286dac7bd20
Marc Watters <marc.watters@proton.me>
2025-01-31 15:19:11 -0500

    17 - rfxr: extract getPost and newPost logic

--- 17-reading_files/blogposts/blogposts.go
+++ 17-reading_files/blogposts/blogposts.go
@@ -1,6 +1,7 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
@@ -15,8 +16,33 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			// todo: needs clarification, should we totally fail if
+			// one file fails? or just ignore?
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile fs.File) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, nil
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
