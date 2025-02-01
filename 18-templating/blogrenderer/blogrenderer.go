1c57739f66e3a06c14a468d9c70a829b4d2f5b5b
Marc Watters <marc.watters@proton.me>
2025-02-01 18:34:46 -0500

    18 - rfxr: utilize embedded templates

--- 18-templating/blogrenderer/blogrenderer.go
+++ 18-templating/blogrenderer/blogrenderer.go
@@ -16,7 +16,7 @@ func Render(w io.Writer, p blogposts.Post) error {
 		return err
 	}
 
-	if err := templ.Execute(w, p); err != nil {
+	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
 
