cd531d2fbe82e878aff398b878f12e86c24928e0
Marc Watters <marc.watters@proton.me>
2025-02-01 17:57:37 -0500

    18 - fix: have Render() generate all fields of blog post

--- 18-templating/blogrenderer_test.go
+++ 18-templating/blogrenderer_test.go
@@ -23,10 +23,12 @@ func TestRender(t *testing.T) {
 		}
 
 		got := buf.String()
-		want := "<h1>hello world</h1>"
+		want := `<h1>hello world</h1>
+<p>This is a description</p>
+Tags: <ul><li>go</li><li>tdd</li></ul>`
 
 		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
+			t.Errorf("\n\ngot:\n\n'%s'\n\nwant:\n\n'%s'", got, want)
 		}
 	})
 }
