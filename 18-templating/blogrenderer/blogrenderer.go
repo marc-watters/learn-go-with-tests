6e8c4a1048977dfc2e3be0153fbc886b9fd88df2
Marc Watters <marc.watters@proton.me>
2025-02-02 11:49:10 -0500

    18 - feat: implement parsing for Post Body

--- 18-templating/blogrenderer/blogrenderer.go
+++ 18-templating/blogrenderer/blogrenderer.go
@@ -5,13 +5,17 @@ import (
 	"html/template"
 	"io"
 	"learn-go-with-tests/v2/17-reading_files/blogposts"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 //go:embed "templates/*"
 var postTemplates embed.FS
 
 type PostRenderer struct {
-	templ *template.Template
+	templ    *template.Template
+	mdParser *parser.Parser
 }
 
 func NewPostRenderer() (*PostRenderer, error) {
@@ -20,13 +24,27 @@ func NewPostRenderer() (*PostRenderer, error) {
 		return nil, err
 	}
 
-	return &PostRenderer{templ: templ}, nil
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
-	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []blogposts.Post) error {
 	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+type postViewModel struct {
+	blogposts.Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p blogposts.Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module learn-go-with-tests/v2
 
 go 1.23.4
 
-require github.com/approvals/go-approval-tests v1.1.0
+require (
+	github.com/approvals/go-approval-tests v1.1.0
+	github.com/gomarkdown/markdown v0.0.0-20241205020045-f7e15b2f3e62
+)
