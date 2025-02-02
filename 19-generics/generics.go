7897db87e0f2f66fbbc25d7e46117dc7cde4f97b
Marc Watters <marc.watters@proton.me>
2025-02-02 18:17:46 -0500

    19 - feat: implement StackOfInts.Pop()

--- 19-generics/generics.go
+++ 19-generics/generics.go
@@ -11,3 +11,14 @@ func (s *StackOfInts) Push(value int) {
 func (s *StackOfInts) IsEmpty() bool {
 	return len(s.values) == 0
 }
+
+func (s *StackOfInts) Pop() (int, bool) {
+	if s.IsEmpty() {
+		return 0, false
+	}
+
+	index := len(s.values) - 1
+	el := s.values[index]
+	s.values = s.values[:index]
+	return el, true
+}
