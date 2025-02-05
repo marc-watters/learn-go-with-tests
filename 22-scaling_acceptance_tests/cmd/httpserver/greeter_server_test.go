edd29560cb85dab0f67506763d13001f6e0845bf
Marc Watters <marc.watters@proton.me>
2025-02-05 12:01:47 -0500

    22 - rfxr: to extract logic for running acceptance test docker container

--- 22-scaling_acceptance_tests/cmd/httpserver/greeter_server_test.go
+++ 22-scaling_acceptance_tests/cmd/httpserver/greeter_server_test.go
@@ -1,45 +1,25 @@
 package main_test
 
 import (
-	"context"
+	"fmt"
+	"go-specs-greet/v2/adapters"
 	"go-specs-greet/v2/adapters/httpserver"
 	"go-specs-greet/v2/specifications"
 	"net/http"
 	"testing"
 	"time"
-
-	"github.com/alecthomas/assert/v2"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	ctx := context.Background()
+	var (
+		port           = "8080"
+		dockerFilePath = "./cmd/httpserver/Dockerfile"
+		baseURL        = fmt.Sprintf("http://localhost:%s", port)
+		driver         = httpserver.Driver{BaseURL: baseURL, Client: &http.Client{
+			Timeout: 1 * time.Second,
+		}}
+	)
 
-	req := testcontainers.ContainerRequest{
-		FromDockerfile: testcontainers.FromDockerfile{
-			Context:    "../../.",
-			Dockerfile: "./cmd/httpserver/Dockerfile",
-			// set to false if you want less spam, but this is helpful if you're
-			// having troubles
-			PrintBuildLog: true,
-		},
-		ExposedPorts: []string{"8080:8080"},
-		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
-	}
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	assert.NoError(t, err)
-	t.Cleanup(func() {
-		assert.NoError(t, container.Terminate(ctx))
-	})
-
-	client := http.Client{
-		Timeout: 1 * time.Second,
-	}
-
-	driver := httpserver.Driver{BaseURL: "http://localhost:8080", Client: &client}
+	adapters.StartDockerServer(t, port, dockerFilePath)
 	specifications.GreetSpecification(t, driver)
 }
