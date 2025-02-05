a50953fc71c2b75087700f6a6342b1fb862ff2ed
Marc Watters <marc.watters@proton.me>
2025-02-05 14:27:50 -0500

    22 - rfxr: to consolidate Dockerfiles between http/gRPC servers

--- 22-scaling_acceptance_tests/adapters/docker.go
+++ 22-scaling_acceptance_tests/adapters/docker.go
@@ -15,7 +15,7 @@ import (
 func StartDockerServer(
 	t testing.TB,
 	port string,
-	dockerFilePath string,
+	binToBuild string,
 ) {
 	t.Helper()
 
@@ -24,7 +24,8 @@ func StartDockerServer(
 	req := testcontainers.ContainerRequest{
 		FromDockerfile: testcontainers.FromDockerfile{
 			Context:       "../../.",
-			Dockerfile:    dockerFilePath,
+			Dockerfile:    "Dockerfile",
+			BuildArgs:     map[string]*string{"bin_to_build": &binToBuild},
 			PrintBuildLog: true,
 		},
 		ExposedPorts: []string{fmt.Sprintf("%s:%s", port, port)},
