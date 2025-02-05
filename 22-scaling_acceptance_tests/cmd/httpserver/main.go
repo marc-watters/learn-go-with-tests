6b2fad7b532dbb6ab630270351bbd6cd73d08ce9
Marc Watters <marc.watters@proton.me>
2025-02-05 10:40:56 -0500

    22 - rfxr: to extract http handler

--- 22-scaling_acceptance_tests/cmd/httpserver/main.go
+++ 22-scaling_acceptance_tests/cmd/httpserver/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	gsg "go-specs-greet/v2"
 )
 
 func main() {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world")
-	})
+	handler := http.HandlerFunc(gsg.Handler)
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
