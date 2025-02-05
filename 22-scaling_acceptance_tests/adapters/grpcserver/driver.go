e046e74182a239b3fe7ca7a59eda90f04135bd5f
Marc Watters <marc.watters@proton.me>
2025-02-05 13:56:48 -0500

    22 - add: implement gRPC driver Greet()

--- 22-scaling_acceptance_tests/adapters/grpcserver/driver.go
+++ 22-scaling_acceptance_tests/adapters/grpcserver/driver.go
@@ -1,9 +1,31 @@
 package grpcserver
 
+import (
+	context "context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 type Driver struct {
 	Addr string
 }
 
 func (d Driver) Greet(name string) (string, error) {
-	return "", nil
+	// TODO: we shouldn't redial every time we call greet, refactor out when we're green
+	conn, err := grpc.Dial(d.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	client := NewGreeterClient(conn)
+	greeting, err := client.Greet(context.Background(), &GreetRequest{
+		Name: name,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return greeting.Message, nil
 }
--- 22-scaling_acceptance_tests/cmd/grpcserver/main.go
+++ 22-scaling_acceptance_tests/cmd/grpcserver/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"go-specs-greet/v2/adapters/grpcserver"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	interactions "go-specs-greet/v2/domain/interactions"
+)
 
 func main() {
-	fmt.Println("implement me")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type GreetServer struct {
+	grpcserver.UnimplementedGreeterServer
+}
+
+func (g GreetServer) Greet(ctx context.Context, request *grpcserver.GreetRequest) (*grpcserver.GreetReply, error) {
+	return &grpcserver.GreetReply{Message: interactions.Greet(request.Name)}, nil
 }
