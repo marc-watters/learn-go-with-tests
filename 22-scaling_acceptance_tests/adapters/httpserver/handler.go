0c2da36c04afd7caa9b8e9283086541fd30d41c1
Marc Watters <marc.watters@proton.me>
2025-02-05 19:33:59 -0500

    22 - rfxr: to consolidate http handler via muxer

--- 22-scaling_acceptance_tests/adapters/httpserver/handler.go
+++ 22-scaling_acceptance_tests/adapters/httpserver/handler.go
@@ -7,7 +7,21 @@ import (
 	interactions "go-specs-greet/v2/domain/interactions"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	fmt.Fprint(w, interactions.Greet(name))
+const (
+	greetPath     = "/greet"
+	introducePath = "/introduce"
+)
+
+func NewHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc(greetPath, replyWith(interactions.Greet))
+	mux.HandleFunc(introducePath, replyWith(interactions.Introduce))
+	return mux
+}
+
+func replyWith(f func(name string) (interaction string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		fmt.Fprint(w, f(name))
+	}
 }
--- 22-scaling_acceptance_tests/cmd/httpserver/main.go
+++ 22-scaling_acceptance_tests/cmd/httpserver/main.go
@@ -7,8 +7,5 @@ import (
 )
 
 func main() {
-	handler := http.HandlerFunc(httpserver.Handler)
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", httpserver.NewHandler()))
 }
