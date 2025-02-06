dc6da71fcdc1ea88372c587c98b56a46db1966db
Marc Watters <marc.watters@proton.me>
2025-02-05 19:32:07 -0500

    22 - add: specification IntroduceAdapter()

--- 22-scaling_acceptance_tests/specifications/adapters.go
+++ 22-scaling_acceptance_tests/specifications/adapters.go
@@ -5,3 +5,9 @@ type GreetAdapter func(name string) string
 func (g GreetAdapter) Greet(name string) (string, error) {
 	return g(name), nil
 }
+
+type IntroduceAdapter func(name string) string
+
+func (i IntroduceAdapter) Introduce(name string) (string, error) {
+	return i(name), nil
+}
